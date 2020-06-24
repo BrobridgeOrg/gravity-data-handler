96af1fc3b43f9a24ca5d46f1313b63a969d3d3d9
jhe <jhe@brobridge.com>
2020-06-24 11:55:36 +0800

    reconnect to event server

--- app/eventbus/eventbus.go
+++ app/eventbus/eventbus.go
@@ -1,21 +1,24 @@
 package eventbus
 
 import (
-	log "github.com/sirupsen/logrus"
+	"time"
+
+	nats "github.com/nats-io/nats.go"
 	stan "github.com/nats-io/stan.go"
+	log "github.com/sirupsen/logrus"
 )
 
 type EventBus struct {
-	host string
-	clusterID string
+	host       string
+	clusterID  string
 	clientName string
-	client stan.Conn
+	client     stan.Conn
 }
 
 func CreateConnector(host string, clusterID string, clientName string) *EventBus {
 	return &EventBus{
-		host: host,
-		clusterID: clusterID,
+		host:       host,
+		clusterID:  clusterID,
 		clientName: clientName,
 	}
 }
@@ -23,13 +26,26 @@ func CreateConnector(host string, clusterID string, clientName string) *EventBus
 func (eb *EventBus) Connect() error {
 
 	log.WithFields(log.Fields{
-		"host": eb.host,
+		"host":       eb.host,
 		"clientName": eb.clientName,
-		"clusterID": eb.clusterID,
+		"clusterID":  eb.clusterID,
 	}).Info("Connecting to event server")
 
 	// Connect to queue server
-	sc, err := stan.Connect(eb.clusterID, eb.clientName, stan.NatsURL(eb.host))
+	nc, err := nats.Connect(eb.host,
+		nats.MaxReconnects(-1),
+		nats.PingInterval(10*time.Second),
+		nats.MaxPingsOutstanding(3),
+	)
+	if err != nil {
+		return err
+	}
+
+	nc.SetReconnectHandler(func(rcb *nats.Conn) {
+		log.Info("Reconnecting to eventbus server ...")
+	})
+
+	sc, err := stan.Connect(eb.clusterID, eb.clientName, stan.NatsConn(nc))
 	if err != nil {
 		return err
 	}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/golang/protobuf v1.3.4
 	github.com/nats-io/nats-streaming-server v0.17.0 // indirect
+	github.com/nats-io/nats.go v1.9.1
 	github.com/nats-io/stan.go v0.6.0
 	github.com/prometheus/common v0.4.0
 	github.com/sirupsen/logrus v1.4.2
